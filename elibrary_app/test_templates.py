5b686d07738fe2059b92cccc1c0a72484803f3fd
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-19 21:50:03 +0200

    Add tests for templates

---
+++ elibrary_app/test_templates.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class CatalogueTemplateTests(SimpleTestCase):
+
+    def test_homepage_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'E-library Application')
+
+    def test_hompage_does_not_contain_incorrect_html(self):
+        response = self.client.get(reverse('home'))
+        self.assertNotContains(response, 'Hello World')
+
