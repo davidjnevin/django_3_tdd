8bb10f5e5d4567ef4b3b7050b46831c863338691
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-05-19 12:57:30 +0200

    Add the URLs

--- elibrary_app/urls.py
+++ elibrary_app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('user/', views.user, name='user'),
+    path('', views.home, name='home'),
 ]
